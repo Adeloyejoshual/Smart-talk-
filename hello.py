f3767bd9accbce86430b244552e8105aedd92db4
Adeloye Joshua <adeloyejoshua2020@gmail.com>
2025-07-20 00:04:31 +0100

    new commit message

---
+++ hello.py
@@ -0,0 +1,1 @@
+print("Hello, Joshua! You're on your way to becoming a Python Pro!")
---
+++ python day1.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+What is your name? Joshua
+How old are you? 22
+Hello Joshua! You are 22 years old.
+
