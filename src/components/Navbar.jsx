c40168b4584cfc091960973cbdc4ffb256792c0a
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-04-23 22:33:34 +0100

    Create src/components/Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="p-4 bg-black text-white text-center font-bold">
+      AllDownloader
+    </nav>
+  );
+};
+
+export default Navbar;
