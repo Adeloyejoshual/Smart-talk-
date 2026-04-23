da90d8c424396a37037c00f0a401acd825200f3b
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-04-23 22:33:16 +0100

    Create src/components/Loader.jsx

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <p className="text-center mt-6 text-gray-500">
+      Processing...
+    </p>
+  );
+};
+
+export default Loader;
