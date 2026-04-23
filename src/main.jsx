48946d85d9616042872b7d7fdd8e9e009fc8d5b4
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-04-24 00:02:58 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-const rootElement = document.getElementById("root");
-
-// Safety check (prevents blank silent failure)
-if (!rootElement) {
-  throw new Error("Root element not found. Check index.html for <div id='root'></div>");
-}
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
