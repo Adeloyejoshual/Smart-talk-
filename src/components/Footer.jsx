ede2a4cab49e232c3f28ab810248669dd70cf593
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-04-23 22:34:23 +0100

    Create src/components/Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-center p-4 text-gray-500 mt-10">
+      © 2026 All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
